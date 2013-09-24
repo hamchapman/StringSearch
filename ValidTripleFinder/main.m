cafb0fcb607f605a6bc11d02dbdf5c3d0a03bf55
Hamilton Chapman <hamchapman@gmail.com>
2013-09-24 15:22:05 +0100

    Initial Commit

---
+++ ValidTripleFinder/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ValidTripleFinder
+//
+//  Created by Hamilton Chapman on 24/09/2013.
+//  Copyright (c) 2013 Hamilton Chapman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
