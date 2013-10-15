66c2c60af826add25e9bce35ea322ad16355a304
Hamilton Chapman <hamchapman@gmail.com>
2013-10-15 23:04:10 +0100

    Refactored main

--- ValidTripleFinder/main.m
+++ ValidTripleFinder/main.m
@@ -27,10 +27,27 @@ int main(int argc, const char * argv[])
         NSMutableArray *doubles = [[NSMutableArray alloc] init];
         NSMutableArray *triples = [[NSMutableArray alloc] init];
         NSMutableArray *quads = [[NSMutableArray alloc] init];
-
-        NSMutableArray *validDoubles = [[NSMutableArray alloc] init];
-        NSMutableArray *validTriples = [[NSMutableArray alloc] init];
-        NSMutableArray *validQuads = [[NSMutableArray alloc] init];
+        
+        NSMutableArray *remedialDoubles = [[NSMutableArray alloc] init];
+        NSMutableArray *easyDoubles = [[NSMutableArray alloc] init];
+        NSMutableArray *mediumDoubles = [[NSMutableArray alloc] init];
+        NSMutableArray *hardDoubles = [[NSMutableArray alloc] init];
+        NSMutableArray *insaneDoubles = [[NSMutableArray alloc] init];
+        NSMutableArray *impossibleDoubles = [[NSMutableArray alloc] init];
+        
+        NSMutableArray *remedialTriples = [[NSMutableArray alloc] init];
+        NSMutableArray *easyTriples = [[NSMutableArray alloc] init];
+        NSMutableArray *mediumTriples = [[NSMutableArray alloc] init];
+        NSMutableArray *hardTriples = [[NSMutableArray alloc] init];
+        NSMutableArray *insaneTriples = [[NSMutableArray alloc] init];
+        NSMutableArray *impossibleTriples = [[NSMutableArray alloc] init];
+        
+        NSMutableArray *remedialQuads = [[NSMutableArray alloc] init];
+        NSMutableArray *easyQuads = [[NSMutableArray alloc] init];
+        NSMutableArray *mediumQuads = [[NSMutableArray alloc] init];
+        NSMutableArray *hardQuads = [[NSMutableArray alloc] init];
+        NSMutableArray *insaneQuads = [[NSMutableArray alloc] init];
+        NSMutableArray *impossibleQuads = [[NSMutableArray alloc] init];
 
         
         for(NSString *letter in alphabet)
@@ -88,7 +105,32 @@ int main(int argc, const char * argv[])
             {
                 doub = [doub stringByAppendingString:@" "];
                 doub = [doub stringByAppendingString:(@"%lu", numOfOccString)];
-                [validDoubles addObject:doub];
+                
+                if(numberOfOccurrences > 20000)
+                {
+                    [remedialDoubles addObject:doub];
+                }
+                else if (numberOfOccurrences > 10000)
+                {
+                    [easyDoubles addObject:doub];
+                }
+                else if (numberOfOccurrences > 5000)
+                {
+                    [mediumDoubles addObject:doub];
+                }
+                else if (numberOfOccurrences > 1000)
+                {
+                    [hardDoubles addObject:doub];
+                }
+                else if (numberOfOccurrences > 500)
+                {
+                    [insaneDoubles addObject:doub];
+                }
+                else
+                {
+                    [impossibleDoubles addObject:doub];
+                }
+
             }
         }
         
@@ -110,10 +152,36 @@ int main(int argc, const char * argv[])
             {
                 triple = [triple stringByAppendingString:@" "];
                 triple = [triple stringByAppendingString:(@"%lu", numOfOccString)];
-                [validTriples addObject:triple];
+                
+                if(numberOfOccurrences > 10000)
+                {
+                    [remedialTriples addObject:triple];
+                }
+                else if (numberOfOccurrences > 5000)
+                {
+                    [easyTriples addObject:triple];
+                }
+                else if (numberOfOccurrences > 1000)
+                {
+                    [mediumTriples addObject:triple];
+                }
+                else if (numberOfOccurrences > 250)
+                {
+                    [hardTriples addObject:triple];
+                }
+                else if (numberOfOccurrences > 10)
+                {
+                    [insaneTriples addObject:triple];
+                }
+                else
+                {
+                    [impossibleTriples addObject:triple];
+                }
+                
             }
+
         }
-        
+
         for(NSString __strong *quad in quads)
         {
             NSInteger numberOfOccurrences = 0;
@@ -132,88 +200,112 @@ int main(int argc, const char * argv[])
             {
                 quad = [quad stringByAppendingString:@" "];
                 quad = [quad stringByAppendingString:(@"%lu", numOfOccString)];
-                [validQuads addObject:quad];
+                
+                if(numberOfOccurrences > 1500)
+                {
+                    [remedialQuads addObject:quad];
+                }
+                else if (numberOfOccurrences > 500)
+                {
+                    [easyQuads addObject:quad];
+                }
+                else if (numberOfOccurrences > 100)
+                {
+                    [mediumQuads addObject:quad];
+                }
+                else if (numberOfOccurrences > 22)
+                {
+                    [hardQuads addObject:quad];
+                }
+                else if (numberOfOccurrences > 7)
+                {
+                    [insaneQuads addObject:quad];
+                }
+                else
+                {
+                    [impossibleQuads addObject:quad];
+                }
             }
         }
-        
-        NSString *listOfValidDoubles = [NSString stringWithFormat:@"%@", [validDoubles componentsJoinedByString:@"\n"]];
+            
         
         NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
         NSString *docDir = [paths objectAtIndex: 0];
-        NSString *doublesFile = [docDir stringByAppendingPathComponent: @"ValidDoubles.txt"];
         
-        [listOfValidDoubles writeToFile: doublesFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
-
-        NSString *listOfValidTriples = [NSString stringWithFormat:@"%@", [validTriples componentsJoinedByString:@"\n"]];
-        NSString *triplesFile = [docDir stringByAppendingPathComponent: @"ValidTriples.txt"];
+        NSString *listOfRemedialDoubles = [NSString stringWithFormat:@"%@", [remedialDoubles componentsJoinedByString:@"\n"]];
+        NSString *remedialDoublesFile = [docDir stringByAppendingPathComponent: @"RemedialDoubles.txt"];
+        [listOfRemedialDoubles writeToFile: remedialDoublesFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
         
-        [listOfValidTriples writeToFile: triplesFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
-
-        NSString *listOfValidQuads = [NSString stringWithFormat:@"%@", [validQuads componentsJoinedByString:@"\n"]];
-        NSString *quadsFile = [docDir stringByAppendingPathComponent: @"ValidQuads.txt"];
+        NSString *listOfEasyDoubles = [NSString stringWithFormat:@"%@", [easyDoubles componentsJoinedByString:@"\n"]];
+        NSString *easyDoublesFile = [docDir stringByAppendingPathComponent: @"EasyDoubles.txt"];
+        [listOfEasyDoubles writeToFile: easyDoublesFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
         
-        [listOfValidQuads writeToFile: quadsFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
+        NSString *listOfMediumDoubles = [NSString stringWithFormat:@"%@", [mediumDoubles componentsJoinedByString:@"\n"]];
+        NSString *mediumDoublesFile = [docDir stringByAppendingPathComponent: @"MediumDoubles.txt"];
+        [listOfMediumDoubles writeToFile: mediumDoublesFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
+        
+        NSString *listOfHardDoubles = [NSString stringWithFormat:@"%@", [hardDoubles componentsJoinedByString:@"\n"]];
+        NSString *hardDoublesFile = [docDir stringByAppendingPathComponent: @"HardDoubles.txt"];
+        [listOfHardDoubles writeToFile: hardDoublesFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
+        
+        NSString *listOfInsaneDoubles = [NSString stringWithFormat:@"%@", [insaneDoubles componentsJoinedByString:@"\n"]];
+        NSString *insaneDoublesFile = [docDir stringByAppendingPathComponent: @"InsaneDoubles.txt"];
+        [listOfInsaneDoubles writeToFile: insaneDoublesFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
+        
+        NSString *listOfImpossibleDoubles = [NSString stringWithFormat:@"%@", [impossibleDoubles componentsJoinedByString:@"\n"]];
+        NSString *impossibleDoublesFile = [docDir stringByAppendingPathComponent: @"ImpossibleDoubles.txt"];
+        [listOfImpossibleDoubles writeToFile: impossibleDoublesFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
+        
+        NSString *listOfRemedialTriples = [NSString stringWithFormat:@"%@", [remedialTriples componentsJoinedByString:@"\n"]];
+        NSString *remedialTriplesFile = [docDir stringByAppendingPathComponent: @"RemedialTriples.txt"];
+        [listOfRemedialTriples writeToFile: remedialTriplesFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
+        
+        NSString *listOfEasyTriples = [NSString stringWithFormat:@"%@", [easyTriples componentsJoinedByString:@"\n"]];
+        NSString *easyTriplesFile = [docDir stringByAppendingPathComponent: @"EasyTriples.txt"];
+        [listOfEasyTriples writeToFile: easyTriplesFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
+        
+        NSString *listOfMediumTriples= [NSString stringWithFormat:@"%@", [mediumTriples componentsJoinedByString:@"\n"]];
+        NSString *mediumTriplesFile = [docDir stringByAppendingPathComponent: @"MediumTriples.txt"];
+        [listOfMediumTriples writeToFile: mediumTriplesFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
+        
+        NSString *listOfHardTriples = [NSString stringWithFormat:@"%@", [hardTriples componentsJoinedByString:@"\n"]];
+        NSString *hardTriplesFile = [docDir stringByAppendingPathComponent: @"HardTriples.txt"];
+        [listOfHardTriples writeToFile: hardTriplesFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
+        
+        NSString *listOfInsaneTriples = [NSString stringWithFormat:@"%@", [insaneTriples componentsJoinedByString:@"\n"]];
+        NSString *insaneTriplesFile = [docDir stringByAppendingPathComponent: @"InsaneTriples.txt"];
+        [listOfInsaneTriples writeToFile: insaneTriplesFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
+        
+        NSString *listOfImpossibleTriples = [NSString stringWithFormat:@"%@", [impossibleTriples componentsJoinedByString:@"\n"]];
+        NSString *impossibleTriplesFile = [docDir stringByAppendingPathComponent: @"ImpossibleTriples.txt"];
+        [listOfImpossibleTriples writeToFile: impossibleTriplesFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
+        
+        NSString *listOfRemedialQuads = [NSString stringWithFormat:@"%@", [remedialQuads componentsJoinedByString:@"\n"]];
+        NSString *remedialQuadsFile = [docDir stringByAppendingPathComponent: @"RemedialQuads.txt"];
+        [listOfRemedialQuads writeToFile: remedialQuadsFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
+        
+        NSString *listOfEasyQuads = [NSString stringWithFormat:@"%@", [easyQuads componentsJoinedByString:@"\n"]];
+        NSString *easyQuadsFile = [docDir stringByAppendingPathComponent: @"EasyQuads.txt"];
+        [listOfEasyQuads writeToFile: easyQuadsFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
+        
+        NSString *listOfMediumQuads = [NSString stringWithFormat:@"%@", [mediumQuads componentsJoinedByString:@"\n"]];
+        NSString *mediumQuadsFile = [docDir stringByAppendingPathComponent: @"MediumQuads.txt"];
+        [listOfMediumQuads writeToFile: mediumQuadsFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
+        
+        NSString *listOfHardQuads = [NSString stringWithFormat:@"%@", [hardQuads componentsJoinedByString:@"\n"]];
+        NSString *hardQuadsFile = [docDir stringByAppendingPathComponent: @"HardQuads.txt"];
+        [listOfHardQuads writeToFile: hardQuadsFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
+        
+        NSString *listOfInsaneQuads = [NSString stringWithFormat:@"%@", [insaneQuads componentsJoinedByString:@"\n"]];
+        NSString *insaneQuadsFile = [docDir stringByAppendingPathComponent: @"InsaneQuads.txt"];
+        [listOfInsaneQuads writeToFile: insaneQuadsFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
+        
+        NSString *listOfImpossibleQuads = [NSString stringWithFormat:@"%@", [impossibleQuads componentsJoinedByString:@"\n"]];
+        NSString *impossibleQuadsFile = [docDir stringByAppendingPathComponent: @"ImpossibleQuads.txt"];
+        [listOfImpossibleQuads writeToFile: impossibleQuadsFile atomically: NO encoding:NSUTF8StringEncoding error:NULL];
         
     }
     return 0;
-}
-
-
-NSMutableArray *difficultyArrayCreator(Difficulty chosenDifficulty, NSDictionary *tripleOccurrences)
-{
-    NSMutableArray *triples = [[NSMutableArray alloc] init];
-    
-    int upperLimit = 0;
-    int lowerLimit = 0;
-    
-    switch (chosenDifficulty)
-    {
-        case remedial:
-            upperLimit = 100000;
-            lowerLimit = 10000;
-            break;
-        case easy:
-            upperLimit = 10000;
-            lowerLimit = 5000;
-            break;
-        case medium:
-            upperLimit = 5000;
-            lowerLimit = 1000;
-            break;
-        case hard:
-            upperLimit = 1000;
-            lowerLimit = 250;
-            break;
-        case insane:
-            upperLimit = 250;
-            lowerLimit = 10;
-            break;
-        case impossible:
-            upperLimit = 10;
-            lowerLimit = 0;
-            break;
-        default:
-            upperLimit = 100000;
-            lowerLimit = 0;
-            break;
-    }
-    
-    for(NSString *key in tripleOccurrences)
-    {
-        NSNumberFormatter * f = [[NSNumberFormatter alloc] init];
-        [f setNumberStyle:NSNumberFormatterDecimalStyle];
-        NSNumber * numOfOccs = [f numberFromString:tripleOccurrences[key]];
-        
-        NSNumber *lowLimit = [NSNumber numberWithInteger:lowerLimit];
-        NSNumber *uppLimit = [NSNumber numberWithInteger:upperLimit];
-        
-        if(numOfOccs > lowLimit && numOfOccs < uppLimit)
-        {
-            [triples addObject:key];
-        }
-    }
-    
-    return triples;
 }
 
 NSInteger numberOfLettersInSet(NSString *setOfLetters)
